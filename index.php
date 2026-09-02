e83acfd93f4b9a3dce334d4004d4dab9b3301847
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2026-09-02 23:25:01 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6360,7 +6360,6 @@ $pageDesc = 'A lightweight, single-file self-hosted cloud drive and media galler
               <svg viewBox="0 0 24 24"><path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"/></svg>
             </button>
             <div style="width:1px; height:20px; background:var(--md-sys-color-outline-variant); margin:0 0.1rem;"></div>
-            <div style="width:1px; height:20px; background:var(--md-sys-color-outline-variant); margin:0 0.1rem;"></div>
             <button class="btn-icon" id="btn-select-all" title="Select All / Deselect All (Ctrl+A)">
               <svg viewBox="0 0 24 24"><path d="M18 7l-1.41-1.41-6.34 6.34 1.41 1.41L18 7zm4.24-1.41L11.66 16.17 7.48 12l-1.41 1.41L11.66 19l12-12-1.42-1.41zM.41 13.41L6 19l1.41-1.41L1.83 12 .41 13.41z"/></svg>
             </button>
